ff0b7f7df54640164b608d6b8ba2f66f9a8adb83
sergeyvasylyev <s.vasylyev@gmail.com>
2019-02-07 09:43:12 +0200

    BubbleSort_2

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -4,7 +4,22 @@ public class BubbleSort {
 
     public static void main(String[] args) {
         int[] ints1 = {5, 7, 1, 3, 2, 0};
-        BubbleSortWhile(ints1);
+        BubbleSort(ints1);
+    }
+
+    public static  void BubbleSort(int[] ints){
+        int intsLength = ints.length;
+        int a = 0;
+        for(int i=0; i < intsLength; i++){
+            for(int j=1; j < (intsLength-i); j++){
+                if(ints[j-1] > ints[j]){
+                    a = ints[j-1];
+                    ints[j-1] = ints[j];
+                    ints[j] = a;
+                }
+            }
+        }
+        System.out.println("Bubble sort "+Arrays.toString(ints));
     }
 
     public static void BubbleSortWhile(int[] ints){
